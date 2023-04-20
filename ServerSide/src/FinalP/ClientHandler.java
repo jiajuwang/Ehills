0c59c885ca401a748e12671dc6e0d55275dc4772
jiajuwang <wangjeffrey4@gmail.com>
2023-04-20 16:35:59 -0500

    ServerSide

--- ServerSide/src/FinalP/ClientHandler.java
+++ ServerSide/src/FinalP/ClientHandler.java
@@ -10,9 +10,8 @@ import java.io.InputStream;*/
 import java.io.*;
 import java.net.Socket;
 import java.util.Observer;
-
-
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
 
 class ClientHandler implements Runnable, Observer {
@@ -23,12 +22,15 @@ class ClientHandler implements Runnable, Observer {
 	  private BufferedWriter toClient;
 	  private ObjectOutputStream objectOutputputStream;
 	  private FileInformation f= new FileInformation();
-	  
+	  private static Map<String,String> customers = new HashMap<String,String>();
+	 
 
 
 	  protected ClientHandler(Server server, Socket clientSocket) {
 	    this.server = server;
 	    this.clientSocket = clientSocket;
+	    
+	    f.read();
 	    try {
 	      fromClient = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
 	      toClient = new BufferedWriter(new OutputStreamWriter(this.clientSocket.getOutputStream()));
@@ -56,7 +58,7 @@ class ClientHandler implements Runnable, Observer {
 	    String input;
 	    
 	    try {
-	      f.read();
+	      input = fromClient.readLine();
 	      objectOutputputStream.writeObject(f);
 	      while ((input = fromClient.readLine()) != null) {	        
 	        //server.processRequest(input);
@@ -69,9 +71,9 @@ class ClientHandler implements Runnable, Observer {
 
 	  public void process(String s) {
 		  String arr[] = s.split(" ");
-		  String name = arr[0];
-		  double price = Double.parseDouble(arr[1]);
-		  if(f.getName().containsKey(name)) {
+		  //String name = arr[0];
+		  //double price = Double.parseDouble(arr[1]);
+		 /* if(f.getName().containsKey(name)) {
 			  if(price>f.getName().get(name)) {
 				  sendToClient("Congradulations");
 				  synchronized(f) {
@@ -85,6 +87,39 @@ class ClientHandler implements Runnable, Observer {
 		  else {
 			  sendToClient("sold");
 		  }
+	  }*/
+		  String toSend;
+		  if(arr[0].equals("login")) {
+			  if(customers.containsKey(arr[1])) {
+				  if(customers.get(arr[1]).equals(arr[2])) {
+					  toSend = "login success";
+					  sendToClient(toSend);
+				  }
+				  else {
+					  toSend = "login fail";
+					  sendToClient(toSend);
+				  }
+			  }
+			  else {
+				  customers.put(arr[1], arr[2]);
+			  }
+		  }
+		  else if(arr[0].equals("buy")){
+			  Double price = Double.parseDouble(arr[3]);
+			  String itemName = arr[2];
+			  if(f.getName().containsKey(itemName)) {
+				  if(f.getName().get(itemName)<=price) {
+					  toSend = "buy success "+ arr[1]+arr[2]+arr[3]+arr[4];
+					  sendToClient(toSend);
+				  }
+				  else {
+					  toSend = "buy fail low "+ arr[1]+arr[2]+arr[3]+arr[4];
+					  sendToClient(toSend);
+				  }
+			  }
+		  }
+		  		
+	
 	  }
 	  @Override
 	  public void update(Observable o, Object arg) {
